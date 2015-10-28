3739e4681f68e7d42d159400189a17815dbf4ac8
A01154027 <luis.garciadaniel01@gmail.com>
2015-10-28 12:07:46 -0600

    Update WSQ10.cpp

--- WSQ10.cpp
+++ WSQ10.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main (){
+  float array [0];
+  float sum = 0;
+  float average;
+  int i;
+  int y = 0;
+  cout<<"Insert ten values in order to calculate the average of them and the standard deviation."<<endl;
+  for (int i = 0; i < 10; i++){
+    cout<<"Enter value:"<<endl;
+    cin>>array[i];
+  } 
+  for (int i = 0; i < 10; i++){
+    sum = sum + array[i];
+  }
+  average = sum/10;
+  for (int i = 0; i < 10; i++){
+    y = y + ((array [i] - average) * (array[i] - average));
+  }
+  y = y / 9;
+  y = sqrt(y);
+  
+  cout<<"The sum is:"<<sum<<endl;
+  cout<<"The average is:"<<average<<endl;
+  cout<<"The standard deiation is:"<<y<<std::endl;
+  return 0;
+}
