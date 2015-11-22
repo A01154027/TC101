f7e46e3ac6f97f03c2a0a740bb2aacea5c4b53f2
A01154027 <luis.garciadaniel01@gmail.com>
2015-11-21 18:55:33 -0600

    Update WSQ13.cpp

--- WSQ13.cpp
+++ WSQ13.cpp
@@ -3,3 +3,18 @@ using namespace std;
 
 float SQRT(float n){
   float x = n;
+  float y = 1;
+  float e = 0.000001;
+  while(x - y > e){
+    x = (x + y)/2;
+    y = n/x;
+  }return x;
+}
+int main (){
+  float n;
+  cout<<"This program calculates the square root of any given number."<<endl;
+  cout<<"Insert a number."<<endl;
+  cin>>n;
+  cout<<"The square root is:"<<SQRT(n)<<endl;
+  return 0;
+}
